69ecfbdc58a9f1c77b3350a57be0091129f4728c
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-01-05 00:51:45 +0800

    Removed excess code blocks

--- collatzSequence.py
+++ collatzSequence.py
@@ -10,23 +10,10 @@ def collatz(number):
       print(int(number))
 
 while True:
-
-  ## collatzSequence.py
-
-def collatz(number):
-        while number != 1:
-            if number % 2 == 0:
-                number = number / 2
-                print(int(number))
-            else:
-                number = 3 * number + 1
-                print(int(number))
-
-while True:
-    try:
-        print('Enter a number')
-        I = int(input())
-        answer = collatz(I)
-    
-    except ValueError:
-        print('Error: Invalid argument.', 'Enter a number.')
+  try:
+    print('Enter a number')
+    I = int(input())
+    answer = collatz(I)
+  
+  except ValueError:
+    print('Error: Invalid argument.', 'Enter a number.')
