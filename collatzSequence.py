afd6b6679bdccd09a2a918ede297601510ddbce2
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-01-05 00:42:53 +0800

    Create collatz sequence file

---
+++ collatzSequence.py
@@ -0,0 +1,32 @@
+# collatzSequence.py
+
+def collatz(number):
+  while number != 1:
+    if number % 2 == 0:
+      number = number / 2
+      print(int(number))
+    else:
+      number = 3 * number + 1
+      print(int(number))
+
+while True:
+
+  ## collatzSequence.py
+
+def collatz(number):
+        while number != 1:
+            if number % 2 == 0:
+                number = number / 2
+                print(int(number))
+            else:
+                number = 3 * number + 1
+                print(int(number))
+
+while True:
+    try:
+        print('Enter a number')
+        I = int(input())
+        answer = collatz(I)
+    
+    except ValueError:
+        print('Error: Invalid argument.', 'Enter a number.')
